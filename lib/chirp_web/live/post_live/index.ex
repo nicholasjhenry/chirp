908e351d6b161f103e2b688844ed274b039ab6c7
Nicholas Henry <nicholas.henry@gmail.com>
2020-06-26 16:06:25 -0400

    Handle update post event

--- lib/chirp_web/live/post_live/index.ex
+++ lib/chirp_web/live/post_live/index.ex
@@ -8,7 +8,7 @@ defmodule ChirpWeb.PostLive.Index do
   def mount(_params, _session, socket) do
     if connected?(socket), do: Timeline.subscribe()
 
-    {:ok, assign(socket, :posts, list_posts())}
+    {:ok, assign(socket, :posts, list_posts()), temporary_assigns: [posts: []]}
   end
 
   @impl true
@@ -44,6 +44,11 @@ defmodule ChirpWeb.PostLive.Index do
 
   @impl true
   def handle_info({:post_created, post}, socket) do
+    {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
+  end
+
+  @impl true
+  def handle_info({:post_updated, post}, socket) do
     {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
   end
 
